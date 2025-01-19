ee2ba15e830d8852da6e93ae93e2e6f3b91483c2
jhonny-berdeja <jhonny.berdeja.100@gmail.com>
2025-01-19 06:04:48 -0300

    implemented request POST whit basic authentication

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
+import React, { useState } from 'react';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  const handleRequest = async () => {
+    const url = 'https://iam-backend-85dc.onrender.com/login'; 
+
+    const username = 'user'; 
+    const password = 'password'; 
+    const auth = 'Basic ' + btoa(username + ':' + password); 
+
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': auth,
+        },
+        body: JSON.stringify({}), 
+      });
+
+      if (response.ok) {
+        console.log('OK: Request exitosa');
+        setMessage('Solicitud exitosa');
+      } else {
+        console.error('Error: Hubo un problema con la solicitud');
+        setMessage('Hubo un problema con la solicitud');
+      }
+    } catch (error) {
+      console.error('Error: No se pudo realizar la solicitud', error);
+      setMessage('No se pudo realizar la solicitud');
+    }
+  };
+
   return (
-    <div >
-      hola jb!
+    <div>
+      <h1>Hola JB!</h1>
+      <button onClick={handleRequest}>Hacer solicitud POST</button>
+      <p>{message}</p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import './index.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
